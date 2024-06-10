ed64ab5c061c216d333d29fe9e8ba6b51c7e32bb
anh.nguyen0429 <121279445+4nhus@users.noreply.github.com>
2024-06-10 23:11:05 +1000

    feat: add showing, deleting, and synchronising issues

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -9,15 +9,40 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @EnvironmentObject var dataController: DataController
+    
+    var issues: [Issue] {
+        let filter = dataController.selectedFilter ?? .all
+        var allIssues: [Issue]
+        
+        if let tag = filter.tag {
+            allIssues = tag.issues?.allObjects as? [Issue] ?? []
+        } else {
+            let request = Issue.fetchRequest()
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
+        }
+        return allIssues.sorted()
+    }
 
     var body: some View {
-        Text("Content")
+        List {
+            ForEach(issues) { issue in
+                IssueRow(issue: issue)
+            }
+            .onDelete(perform: delete)
+        }
+        .navigationTitle("Issues")
+    }
+    
+    func delete(_ offsets: IndexSet) {
+        for offset in offsets {
+            let issue = issues[offset]
+            dataController.delete(issue)
+        }
     }
 }
 
-
-
 #Preview {
-    ContentView().environment(\.managedObjectContext, DataController.preview.container.viewContext)
+    ContentView().environmentObject(DataController.preview)
 }
