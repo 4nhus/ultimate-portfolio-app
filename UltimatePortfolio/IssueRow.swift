a9e106a77daf33e2ee40f9ac764c942ed935e622
anh.nguyen0429 <121279445+4nhus@users.noreply.github.com>
2024-06-14 09:43:24 +1000

    feat: add accessibility labels and hints

--- UltimatePortfolio/IssueRow.swift
+++ UltimatePortfolio/IssueRow.swift
@@ -32,6 +32,7 @@ struct IssueRow: View {
                 
                 VStack(alignment: .trailing) {
                     Text(issue.issueCreationDate.formatted(date: .numeric, time: .omitted))
+                        .accessibilityLabel(issue.issueCreationDate.formatted(date: .abbreviated, time: .omitted))
                         .font(.subheadline)
                     
                     if issue.completed {
@@ -41,6 +42,7 @@ struct IssueRow: View {
                 }
             }
         }
+        .accessibilityHint(issue.priority == 2 ? "High priority" : "")
     }
 }
 
