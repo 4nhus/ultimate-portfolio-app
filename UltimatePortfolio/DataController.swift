2ac4a602126bcaf292f178428980928956378688
anh.nguyen0429 <121279445+4nhus@users.noreply.github.com>
2024-06-14 19:33:01 +1000

    feat: add i18n

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -201,13 +201,13 @@ class DataController: ObservableObject {
     func newTag() {
         let tag = Tag(context: container.viewContext)
         tag.id = UUID()
-        tag.name = "New Tag"
+        tag.name = NSLocalizedString("New Tag", comment: "Create a new tag")
         save()
     }
     
     func newIssue() {
         let issue = Issue(context: container.viewContext)
-        issue.title = "New Issue"
+        issue.title = NSLocalizedString("New Issue", comment: "Create a new issue")
         issue.creationDate = .now
         issue.priority = 1
         
