f9c3182e3b60b2afab5bd4656b4356ab99b5325b
anh.nguyen0429 <121279445+4nhus@users.noreply.github.com>
2024-06-17 13:08:14 +1000

    feat: add notifications for issue reminders

--- UltimatePortfolio/Editing/IssueView.swift
+++ UltimatePortfolio/Editing/IssueView.swift
@@ -11,6 +11,9 @@ struct IssueView: View {
     @EnvironmentObject var dataController: DataController
     @ObservedObject var issue: Issue
 
+    @State private var showingNotificationsError = false
+    @Environment(\.openURL) var openURL
+
     var body: some View {
         Form {
             Section {
@@ -44,6 +47,18 @@ struct IssueView: View {
                         "Enter the issue description here"), axis: .vertical)
                 }
             }
+
+            Section("Reminders") {
+                Toggle("Show Reminders", isOn: $issue.reminderEnabled.animation())
+
+                if issue.reminderEnabled {
+                    DatePicker(
+                        "Reminder Time",
+                        selection: $issue.issueReminderTime,
+                        displayedComponents: .hourAndMinute
+                    )
+                }
+            }
         }
         .disabled(issue.isDeleted)
         .onReceive(issue.objectWillChange) { _ in
@@ -52,6 +67,39 @@ struct IssueView: View {
         .onSubmit(dataController.save)
         .toolbar {
             IssueViewToolbar(issue: issue)
+        }
+        .alert("Oops!", isPresented: $showingNotificationsError) {
+            Button("Check Settings", action: showAppSettings)
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("There was a problem setting your notification. Please check you have notifications enabled.")
+        }
+        .onChange(of: issue.reminderEnabled) { _, _ in
+            updateReminder()
+        }
+        .onChange(of: issue.reminderTime) { _, _ in
+            updateReminder()
+        }
+    }
+
+    func showAppSettings() {
+        guard let settingsURL = URL(string: UIApplication.openNotificationSettingsURLString) else { return }
+
+        openURL(settingsURL)
+    }
+
+    func updateReminder() {
+        dataController.removeReminders(for: issue)
+
+        Task { @MainActor in
+            if issue.reminderEnabled {
+                let success = await dataController.addReminder(for: issue)
+
+                if success == false {
+                    issue.reminderEnabled = false
+                    showingNotificationsError = true
+                }
+            }
         }
     }
 }
